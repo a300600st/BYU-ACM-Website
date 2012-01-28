fad145307859291dc12abad943ca7fd550a49d7d
Ryan Hansen <ryansh@Ryan-PC.(none)>
2012-01-27 21:05:08 -0700

    Edited contact page.

--- contact.php
+++ contact.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
-<link href="css/sponsor_page.css" rel="stylesheet" type="text/css" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>BYU ACM - Contact</title>
+	<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+	<link href="css/sponsor_page.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>BYU ACM - Contact</title>
 
+	<script language="javascript" type="text/javascript">
+<!--
+/****************************************************
+     Author: Eric King
+     Url: http://redrival.com/eak/index.shtml
+     This script is free to use as long as this info is left in
+     Featured on Dynamic Drive script library (http://www.dynamicdrive.com)
+****************************************************/
+var win=null;
+function NewWindow(mypage,myname,w,h,scroll,pos){
+if(pos=="random"){LeftPosition=(screen.width)?Math.floor(Math.random()*(screen.width-w)):100;TopPosition=(screen.height)?Math.floor(Math.random()*((screen.height-h)-75)):100;}
+if(pos=="center"){LeftPosition=(screen.width)?(screen.width-w)/2:100;TopPosition=(screen.height)?(screen.height-h)/2:100;}
+else if((pos!="center" && pos!="random") || pos==null){LeftPosition=0;TopPosition=20}
+settings='width='+w+',height='+h+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no';
+win=window.open(mypage,myname,settings);}
+-->
+</script>
 </head>
 
 <body>
@@ -14,6 +31,7 @@
 
 <div id="content">
 
+<a href="http://eepurl.com/hebm-/" onclick="NewWindow(this.href,'Subscribe for emails','620','820','no','center');return false" onfocus="this.blur()">Sign up for emails.</a>
 
 </div>
 <div id="footer">
